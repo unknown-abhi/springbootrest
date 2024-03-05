630e2c54159bcbbc482b1169389174b4276b4705
Abhishek Kumar <ak35540@gmail.com>
2024-03-06 02:32:37 +0530

    Spring Boot Rest Demo Project

--- src/main/java/com/example/springbootrest/AlienRepository.java
+++ src/main/java/com/example/springbootrest/AlienRepository.java
@@ -3,4 +3,5 @@ package com.example.springbootrest;
 import org.springframework.data.repository.CrudRepository;
 
 public interface AlienRepository extends CrudRepository<Aliens, Integer> {
+
 }
--- src/main/java/com/example/springbootrest/SpringbootrestApplication.java
+++ src/main/java/com/example/springbootrest/SpringbootrestApplication.java
@@ -1,13 +1,15 @@
 package com.example.springbootrest;
 
+import org.apache.catalina.core.ApplicationContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootrestApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootrestApplication.class, args);
-	}
+    public static void main(String[] args) {
+
+        ApplicationContext context = (ApplicationContext) SpringApplication.run(SpringbootrestApplication.class, args);
+    }
 
 }
